4154f78cf5d814c9b299deb3e37bd7d3b89aa53e
matrone <danmatrone@hotmail.com>
2021-10-08 01:02:07 -0300

    ND-9 - Refatoração da class DetectionStage.

--- python/src/DetectionStage.py
+++ python/src/DetectionStage.py
@@ -13,39 +13,7 @@ class DetectionStage:
                 return True
         return False
 
-    def print_expected_detected(self, config_expected_detected):
-        print("Expected to be detected: ", end='')
-        for it in config_expected_detected:
-            print(it, end='')
-            it += 1
-            if it != config_expected_detected[-1]:
-                print(",", end=' ')
-
-    def print_found(self, detected: set, config_expected_detected):
-        print("\nFound: ", end='')
-        for it in detected:
-            print(it, end='')
-            it += 1
-            if it != config_expected_detected[-1]:
-                print(",", end=' ')
-
-    def remove_found_from_expected_detected(self, detected: set, expected_detected):
-        for it in detected:
-            for found in expected_detected:
-                if it == found:
-                    expected_detected.remove(found)
-                    break
-
-    def remove_found_from_detected(self, detected: set, expected_detected):
-        for it in expected_detected:
-            for found in detected:
-                if it == found:
-                    detected.remove(found)
-                    break
-
-    def apply_detectors(self, detectors: list) -> Result:
-        detected = set()
-        config_expected_detected = self._config_file.expected_detected
+    def detection(self, detectors: list, detected: set):
         trial = 1
         for it in self._selfDataset:
             actual: bool = self._geometry.matches(
@@ -56,20 +24,61 @@ class DetectionStage:
                 detected.add(trial)
             trial += 1
 
-        self.print_expected_detected(config_expected_detected)
-        self.print_found(detected, config_expected_detected)
+    def print_expected_detected(self):
+        print("Expected to be detected: ", end='')
+        for it in self._config_file.expected_detected:
+            print(it, end='')
+            it += 1
+            if it != self._config_file.expected_detected[-1]:
+                print(",", end=' ')
 
-        expected_detected = set(config_expected_detected)
+    def print_found(self, detected: set):
+        print("\nFound: ", end='')
+        for it in detected:
+            print(it, end='')
+            it += 1
+            if it != self._config_file.expected_detected[-1]:
+                print(",", end=' ')
+
+    def remove_found_from_expected_detected(self, detected: set, expected_detected: set):
+        for it in detected:
+            for found in expected_detected:
+                if it == found:
+                    expected_detected.remove(found)
+                    break
+
+    def remove_found_from_detected(self, detected: set, expected_detected: list):
+        for it in expected_detected:
+            for found in detected:
+                if it == found:
+                    detected.remove(found)
+                    break
+
+    def calculate_DR(self, result: Result, detected: set):
+        expected_detected = set(self._config_file.expected_detected)
         expected_detected_size = len(expected_detected)
         self.remove_detected(detected, expected_detected)
         new_expected_detected_size = len(expected_detected)
-
-        result = Result()
         result.calculate_DR(expected_detected_size, new_expected_detected_size)
 
-        expected_detected.clear()
-        expected_detected = [value for value in config_expected_detected]
+    def calculate_FAR(self, result: Result, detected: set):
+        expected_detected = [value for value in self._config_file.expected_detected]
+        expected_detected_size = len(expected_detected)
         self.remove_found_from_detected(detected, expected_detected)
         new_detected_size = len(detected)
-        result.calculate_FAR(expected_detected_size, new_detected_size) 
+        result.calculate_FAR(expected_detected_size, new_detected_size)
+
+    def calculate_result(self, detected: set) -> Result:
+        result = Result()
+        self.calculate_DR(result, detected)
+        self.calculate_FAR(result, detected)
         return result
+
+    def apply_detectors(self, detectors: list) -> Result:
+        detected = set()
+
+        self.detection(detectors, detected)
+        self.print_expected_detected()
+        self.print_found(detected)
+
+        return self.calculate_result(detected)
